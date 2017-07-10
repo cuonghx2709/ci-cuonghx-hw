1ab47bd51fcbc6c133c5af1bd89a71616317fd06
DESKTOP-071711U\cuonghx2709 <cuonghx2709@gmail.com>
2017-07-10 10:35:58 +0700

    hw1

---
+++ hw1/src/program.java
@@ -0,0 +1,11 @@
+import game.GameWindow;
+
+/**
+ * Created by cuonghx2709 on 7/8/2017.
+ */
+public class program {
+    public static void main(String[] args) {
+        GameWindow gameWindow = new GameWindow();
+        gameWindow.run();
+    }
+}
