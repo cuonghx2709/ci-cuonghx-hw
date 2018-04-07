8300d7c0ca7446aa7e10bd9aab70b2bde7d81c7b
cuonghx <cuonghx@cuonghxs-MacBook-Pro.local>
2018-04-07 22:23:46 +0700

    test git on mac

---
+++ test/program.java
@@ -0,0 +1,21 @@
+import game.GameWindow;
+import game.Utils;
+import game.bases.BoxCollider;
+import game.bases.MathX;
+import game.bases.Vector2D;
+import tklibs.AudioUtils;
+
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import java.util.ArrayList;
+
+
+/**
+ * Created by cuonghx2709 on 7/8/2017.
+ */
+public class program {
+    public static void main(String[] args) {
+        GameWindow gameWindow = new GameWindow();
+        gameWindow.loop();
+    }
+}
